16b26e30f4d063d8f38f6e59192476db81f7bab6
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-18 23:44:40 -0500

    Add exception interface.

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser Directory - Manage your database with complete ease.
+ * 
+ * @package Kooser\Directory.
+ */
+ 
+namespace Kooser\Directory\Exception;
+
+use Throwable;
+
+/**
+ * The interface for all exceptions.
+ */
+interface ExceptionInterface extends Throwable
+{
+}
