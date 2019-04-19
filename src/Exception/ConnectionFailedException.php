cdb696d04de2a79f4246cee53619beebb67a430f
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-19 01:34:13 -0500

    Add connection closed exception.

--- src/Exception/ConnectionFailedException.php
+++ src/Exception/ConnectionFailedException.php
@@ -1,1 +1,18 @@
+?php
+declare(strict_types=1);
+/**
+ * Kooser Directory - Manage your database with complete ease.
+ * 
+ * @package Kooser\Directory.
+ */
 
+namespace Kooser\Directory\Exception;
+
+use Exception;
+
+/**
+ * If the connection could not be opened.
+ */
+class ConnectionFailedException extends Exception implements ExceptionInterface
+{
+}
