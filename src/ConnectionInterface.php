9b94be793f1e6081637ceb3e0e3dc97afd5b84aa
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-18 23:32:01 -0500

    Add connection manager interface.

---
+++ src/ConnectionInterface.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser PasswordLock - Secure your password using password lock.
+ * 
+ * @package Kooser\PasswordLock.
+ */
+ 
+namespace Kooser\Directory;
+
+/**
+ * The connection manager interface.
+ */
+class ConnectionInterface
+{
+}
