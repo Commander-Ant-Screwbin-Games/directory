b6709721fcae49b01c2419a5169b380b06a64589
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-20 17:57:22 -0500

    Add code.

--- src/DatabaseHandler.php
+++ src/DatabaseHandler.php
@@ -1,1 +1,38 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser Directory - Manage your database with complete ease.
+ * 
+ * @package Kooser\Directory.
+ */
 
+namespace Kooser\Directory;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Create a new database handler.
+ */
+class ConnectionManager implements ConnectionManagerInterface
+{
+
+    /** @var \Kooser\Directory\ConnectionManager $connectionManager The connection manager. */
+    protected $connectionString = \null;
+
+    /**
+     * Construct a new database handler.
+     *
+     * @param \Kooser\Directory\ConnectionManager $connectionManager The connection manager.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(ConnectionManager $connectionManager)
+    {
+        $this->connectionManager = $connectionManager;
+        $this->connectionManager->establishConnection();
+    }
+
+    public function createDatabase(string $databaseName): void
+    {
+    }
+}
