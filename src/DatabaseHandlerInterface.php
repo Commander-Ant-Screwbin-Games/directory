b97465fb2202fe6bafbfd76cfb10cc7e044d3ba1
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-20 17:39:08 -0500

    Add the database handler interface

--- src/DatabaseHandlerInterface.php
+++ src/DatabaseHandlerInterface.php
@@ -1,1 +1,16 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser Directory - Manage your database with complete ease.
+ * 
+ * @package Kooser\Directory.
+ */
+ 
+namespace Kooser\Directory;
 
+/**
+ * The database handler interface.
+ */
+interface DatabaseHandlerInterface
+{
+}
