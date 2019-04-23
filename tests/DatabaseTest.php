b845e5ca13873ac66d15f1e62adebe14692022b0
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-22 22:26:38 -0500

    Test constructor.

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -1,1 +1,32 @@
 
+<?php
+declare(strict_types=1);
+/**
+ * Kooser Directory - A PDO wrapper for secure connections.
+ * 
+ * @package Kooser\Directory.
+ */
+
+namespace Directory\Test;
+
+use Kooser\Directory\ConnectionManager;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test connecion manager functionality.
+ */
+class DatabaseTest extends TestCase
+{
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testConstructor(): void
+    {
+        $dbh = new ConnectionManager([
+            'database_dns'  => 'mysql:host=localhost;dbname=test',
+            'database_user' => 'travis'
+        ]);
+        $this->assertTrue(\true);
+    }
+}
