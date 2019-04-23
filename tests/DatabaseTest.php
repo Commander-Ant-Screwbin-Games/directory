a3f10ef6b0bfd995921a1d8ef44f9f300d4b3a04
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-22 23:04:02 -0500

    Add tests.

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -13,6 +13,7 @@ use Kooser\Directory\Exception\ConnectionClosedException;
 use Kooser\Directory\Exception\ConnectionFailedException;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Error\Error;
 
 /**
  * Test connecion manager functionality.
@@ -59,7 +60,7 @@ class DatabaseTest extends TestCase
         $dbh->establishConnection();
         $this->assertTrue(\true);
         $this->assertTrue(($dbh->getDriver() == 'mysql'));
-        $dbh->closeConnection();
+        $dbh->closeConnectionString();
     }
 
     /**
@@ -73,5 +74,46 @@ class DatabaseTest extends TestCase
             'database_user' => 'kooser'
         ]);
         $dbh->establishConnection();
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testConnectionStringWithNoOpenConnection(): void
+    {
+        $this->expectException(ConnectionClosedException::class);
+        $dbh = new ConnectionManager([
+            'database_dsn'  => 'mysql:host=localhost;dbname=travis_ci_test',
+            'database_user' => 'travis'
+        ]);
+        $conn = $dbh->getConnectionString();
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testConnectionStringWithNoOpenConnectionWithTrigger(): void
+    {
+        $this->expectException(Error::class);
+        $dbh = new ConnectionManager([
+            'database_dsn'  => 'mysql:host=localhost;dbname=travis_ci_test',
+            'database_user' => 'travis'
+        ]);
+        $conn = $dbh->getConnectionString();
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testConnectionStringWithNoOpenConnectionWithTrigger(): void
+    {
+        $dbh = new ConnectionManager([
+            'database_dsn'  => 'mysql:host=localhost;dbname=travis_ci_test',
+            'database_user' => 'travis'
+        ]);
+        $this->assertTrue(\true);
+        $dbh->establishConnection();
+        $this->assertTrue(\true);
+        $conn = $dbh->getConnectionString();
     }
 }
