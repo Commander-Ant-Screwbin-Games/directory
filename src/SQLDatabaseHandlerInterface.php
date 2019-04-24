8c76cb85e6f9228b3dd62a8de41df18077896262
Nicholas <48822466+Kooser6@users.noreply.github.com>
2019-04-24 02:04:46 -0500

    Add interface.

--- src/SQLDatabaseHandlerInterface.php
+++ src/SQLDatabaseHandlerInterface.php
@@ -1,1 +1,16 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser Directory - A PDO wrapper for secure connections.
+ * 
+ * @package Kooser\Directory.
+ */
+ 
+namespace Kooser\Directory;
 
+/**
+ * The sql database handler interface.
+ */
+interface SQLDatabaseHandlerInterface
+{
+}
