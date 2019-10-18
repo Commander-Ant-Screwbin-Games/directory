2ec5cce2b49ca2d2a3ff8597870f5857acd19c6a
Nicholas <48822466+Kooser6@users.noreply.github.com>
2019-10-18 13:45:45 -0400

    Add interface.

---
+++ src/CacheStatementInterface.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser Directory - A PDO wrapper for secure connections.
+ * 
+ * @package Kooser\Framework\Directory.
+ */
+
+namespace Kooser\Framework\Directory;
+
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+
+/**
+ * Create a new database connection.
+ */
+interface CacheStatementInterface
+{
+
+    /**
+     * Construct a new cache statement handler.
+     *
+     * @param \Symfony\Component\Cache\Adapter\AdapterInterface $cacheHandler The cache handler.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(AdapterInterface $cacheHandler);
+
+    /**
+     * Select data.
+     *
+     * @param string $sql       An SQL string.
+     * @param array  $array     Parameters to bind.
+     * @param int    $fetchMode A PDO Fetch mode.
+     *
+     * @return array The selected data.
+     */
+    public function select(string $sql, array $array = array(), int $fetchMode = \PDO::FETCH_ASSOC);
+}
